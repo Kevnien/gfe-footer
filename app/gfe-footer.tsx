ce1f9bdd779c330416bafc7ec7c48f39d6116ce6
Kev <kevinn858@gmail.com>
2025-06-24 17:26:07 -0700

    removed css file

--- app/gfe-footer.tsx
+++ app/gfe-footer.tsx
@@ -1,5 +1,3 @@
-import "./globals.css";
-
 export default function GfeFooter({
   username,
   name,
@@ -9,16 +7,31 @@ export default function GfeFooter({
 }) {
   return (
     <footer>
-      <div className="credits">
+      <div
+        className="credits"
+        style={{
+          color: "#777",
+          position: "fixed",
+          bottom: "24px",
+          left: "24px",
+          fontSize: "11px",
+          textAlign: "center",
+        }}
+      >
         A challenge by
         <a
           href="https://www.greatfrontend.com/projects?ref=challenges"
           target="_blank"
+          style={{ color: "#000", fontWeight: "bold" }}
         >
           GreatFrontEnd Projects
         </a>
         . Built by
-        <a href={`https://www.greatfrontend.com/u/${username}`} target="_blank">
+        <a
+          href={`https://www.greatfrontend.com/u/${username}`}
+          target="_blank"
+          style={{ color: "#000", fontWeight: "bold" }}
+        >
           {name}
         </a>
         .
--- app/kevin-gfe-footer.tsx
+++ app/kevin-gfe-footer.tsx
@@ -1,5 +1,4 @@
 import GfeFooter from "./gfe-footer";
-import "./globals.css";
 
 export default function KevinGfeFooter() {
   return <GfeFooter username="kevinn858" name="Kevin Nguyen" />;
